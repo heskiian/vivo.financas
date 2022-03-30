447705b191389f6496685be2ee19662e5ba9cd3d
Ian Maia <heskiian@gmail.com>
2022-03-30 16:12:19 -0300

    Mapeando as entidades do banco de dados

--- src/main/java/com/vivo/minhasfinancas/model/entity/Usuario.java
+++ src/main/java/com/vivo/minhasfinancas/model/entity/Usuario.java
@@ -1,5 +1,60 @@
 package com.vivo.minhasfinancas.model.entity;
 
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Data
+@Table( name= "usuario", schema = "financas")
 public class Usuario {
+	
+	@Column(name = "id")
+	@GeneratedValue( strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "nome")
+	private String nome;
+	
+	@Column(name = "email")
+
+	private String email;
+	
+	@Column(name = "senha")
+
+	private String senha;
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(email, id, nome, senha);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		return Objects.equals(email, other.email) && Objects.equals(id, other.id) && Objects.equals(nome, other.nome)
+				&& Objects.equals(senha, other.senha);
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario [id=" + id + ", nome=" + nome + ", email=" + email + ", senha=" + senha + "]";
+	}
+	
+	
+	
+	
 
 }
