07042ba422c786e10e1cd7e830ee2fb6d2326a9c
Ian Maia <heskiian@gmail.com>
2022-04-13 14:46:08 -0300

    Implementaçoes finais dos testes de Usuario

--- src/test/java/com/vivo/minhasfinancas/service/UsuarioServiceTest.java
+++ src/test/java/com/vivo/minhasfinancas/service/UsuarioServiceTest.java
@@ -9,9 +9,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import com.vivo.minhasfinancas.exception.AuthError;
 import com.vivo.minhasfinancas.exception.RegraNegocioException;
 import com.vivo.minhasfinancas.model.entity.Usuario;
 import com.vivo.minhasfinancas.model.repository.UsuarioRepository;
@@ -22,37 +24,99 @@ import com.vivo.minhasfinancas.service.impl.UsuarioServiceImpl;
 public class UsuarioServiceTest {
 	
 		
-	
-	UsuarioService service;
+	@SpyBean
+	UsuarioServiceImpl service;
 	
 	@MockBean
 	UsuarioRepository repository;
 	
-	@BeforeEach
-	public void setUp() {
-		service = new UsuarioServiceImpl(repository);
+	@Test
+	public void deveSalvarUmUsuario() {
+		//cenário
+		Mockito.doNothing().when(service).validarEmail(Mockito.anyString());
+		Usuario usuario = Usuario.builder()
+					.id(1l)
+					.nome("nome")
+					.email("email@email.com")
+					.senha("senha").build();
 		
+		Mockito.when(repository.save(Mockito.any(Usuario.class))).thenReturn(usuario);
+		
+		//acao
+		Usuario usuarioSalvo = service.salvarUsuario(new Usuario());
+		
+		//verificao
+		Assertions.assertThat(usuarioSalvo).isNotNull();
+		Assertions.assertThat(usuarioSalvo.getId()).isEqualTo(1l);
+		Assertions.assertThat(usuarioSalvo.getNome()).isEqualTo("nome");
+		Assertions.assertThat(usuarioSalvo.getEmail()).isEqualTo("email@email.com");
+		Assertions.assertThat(usuarioSalvo.getSenha()).isEqualTo("senha");
 		
 	}
 	
 	@Test
-	public void deveAutenticarUmUsuarioComSucesso() {
+	public void naoDeveSalvarUmUsuarioComEmailJaCadastrado() {
 		//cenario
+		String email = "email@email.com";
+		Usuario usuario = Usuario.builder().email(email).build();
+		Mockito.doThrow(RegraNegocioException.class).when(service).validarEmail(email);
+		
+		//acao
+		org.junit.jupiter.api.Assertions
+			.assertThrows(RegraNegocioException.class, () -> service.salvarUsuario(usuario) ) ;
+		
+		//verificacao
+		Mockito.verify( repository, Mockito.never() ).save(usuario);
+	}
+	
+	
+	
+	@Test
+	public void deveAutenticarUmUsuarioComSucesso() {
+		//cenário
 		String email = "email@email.com";
 		String senha = "senha";
 		
 		Usuario usuario = Usuario.builder().email(email).senha(senha).id(1l).build();
-		Mockito.when(repository.findByEmail(email)).thenReturn(Optional.of(usuario));
+		Mockito.when( repository.findByEmail(email) ).thenReturn(Optional.of(usuario));
 		
-		//ação
+		//acao
 		Usuario result = service.autenticar(email, senha);
 		
-		//verificação
+		//verificacao
 		Assertions.assertThat(result).isNotNull();
 		
+	}
+	
+	@Test
+	public void deveLancarErroQuandoNaoEncontraUsuarioCadastradoComOEmail() {
+		//cenario
+		Mockito.when(repository.findByEmail(Mockito.anyString())).thenReturn(Optional.empty());
 		
+		//ação
+		Throwable exception = Assertions.catchThrowable( () -> service.autenticar("email@email.com", "senha") );
+		//verificacao
+				Assertions.assertThat(exception)
+					.isInstanceOf(AuthError.class)
+					.hasMessage("Usuário não encontrado.");
 		
 	}
+	
+
+	@Test
+	public void deveLancarErroQuandoSenhaNaoBater() {
+		//cenario
+		String senha = "senha";
+		Usuario usuario = Usuario.builder().email("email@email.com").senha(senha).build();
+		Mockito.when(repository.findByEmail(Mockito.anyString())).thenReturn(Optional.of(usuario));
+		
+		//acao
+		Throwable exception = Assertions.catchThrowable( () ->  service.autenticar("email@email.com", "123") );
+		Assertions.assertThat(exception).isInstanceOf(AuthError.class).hasMessage("Senha inválida.");
+		
+	}
+	
+	
 	
 	
 	@Test
